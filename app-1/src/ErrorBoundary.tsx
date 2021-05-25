4761df81add6167792f98ca0692ca7c538d5f4c1
Joe Ray <jray@kooth.com>
2021-05-25 09:52:20 +0100

    Only startTransactionOnPageLoad if Sentry hasn't been initialised

--- app-1/src/ErrorBoundary.tsx
+++ app-1/src/ErrorBoundary.tsx
@@ -3,6 +3,12 @@ import { useHistory } from "react-router";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
+declare global {
+  interface Window {
+    sentryInitialised?: boolean;
+  }
+}
+
 const AlertCard: React.FC = () => (
   <div>
     <p>Something went wrong. Please refresh the page and try again.</p>
@@ -15,16 +21,19 @@ const ErrorBoundary: React.FC = ({ children }) => {
   const history = useHistory() as any;
 
   React.useEffect(() => {
+    console.log("window.sentryInitialised", window.sentryInitialised);
     Sentry.init({
       dsn: DSN,
       environment: "live",
       integrations: [
         new Integrations.BrowserTracing({
           routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+          startTransactionOnPageLoad: window.sentryInitialised ? false : true,
         }),
       ],
       tracesSampleRate: 1.0,
     });
+    window.sentryInitialised = true;
   }, []);
 
   return (
